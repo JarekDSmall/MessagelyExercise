fa7eba87641e118df5048ceacd980e28bdb48a96
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-24 20:58:13 -0400

    Update messages.js

--- express-messagely/routes/messages.js
+++ express-messagely/routes/messages.js
@@ -1,30 +1,47 @@
-/** GET /:id - get detail of message.
- *
- * => {message: {id,
- *               body,
- *               sent_at,
- *               read_at,
- *               from_user: {username, first_name, last_name, phone},
- *               to_user: {username, first_name, last_name, phone}}
- *
- * Make sure that the currently-logged-in users is either the to or from user.
- *
- **/
+const Router = require("express").Router;
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const router = new Router();
+
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const message = await Message.get(req.params.id);
+    
+    if (
+      message.from_user.username !== req.user.username &&
+      message.to_user.username !== req.user.username
+    ) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
-/** POST / - post message.
- *
- * {to_username, body} =>
- *   {message: {id, from_username, to_username, body, sent_at}}
- *
- **/
+
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const { to_username, body } = req.body;
+    const from_username = req.user.username;
+    const message = await Message.create({ from_username, to_username, body });
+    return res.status(201).json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
-/** POST/:id/read - mark message as read:
- *
- *  => {message: {id, read_at}}
- *
- * Make sure that the only the intended recipient can mark as read.
- *
- **/
 
+router.post("/:id/read", ensureCorrectUser, async function (req, res, next) {
+  try {
+    const message = await Message.markRead(req.params.id);
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
