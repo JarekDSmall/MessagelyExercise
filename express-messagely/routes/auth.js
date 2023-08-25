ee45906ad1a3b59ee6802e94b9e42796bec43faf
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-24 20:57:37 -0400

    Update auth.js

--- express-messagely/routes/auth.js
+++ express-messagely/routes/auth.js
@@ -1,13 +1,47 @@
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
+const Router = require("express").Router;
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const router = new Router();
+
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const message = await Message.get(req.params.id);
+    
+    if (
+      message.from_user.username !== req.user.username &&
+      message.to_user.username !== req.user.username
+    ) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 
-/** POST /register - register user: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
- */
+
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const { to_username, body } = req.body;
+    const from_username = req.user.username;
+    const message = await Message.create({ from_username, to_username, body });
+    return res.status(201).json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+
+router.post("/:id/read", ensureCorrectUser, async function (req, res, next) {
+  try {
+    const message = await Message.markRead(req.params.id);
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
