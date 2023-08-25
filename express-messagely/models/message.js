7aa849bd60298e0f188f175e3f37f5ae3601ba0a
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-24 20:56:10 -0400

    Update message.js

--- express-messagely/models/message.js
+++ express-messagely/models/message.js
@@ -1,40 +1,29 @@
-/** Message class for message.ly */
-
 const db = require("../db");
 const ExpressError = require("../expressError");
 
-
-/** Message on the site. */
-
 class Message {
-
-  /** register new message -- returns
-   *    {id, from_username, to_username, body, sent_at}
-   */
-
-  static async create({from_username, to_username, body}) {
+  static async create({ from_username, to_username, body }) {
     const result = await db.query(
-        `INSERT INTO messages (
-              from_username,
-              to_username,
-              body,
-              sent_at)
-            VALUES ($1, $2, $3, current_timestamp)
-            RETURNING id, from_username, to_username, body, sent_at`,
-        [from_username, to_username, body]);
-
+      `INSERT INTO messages (
+            from_username,
+            to_username,
+            body,
+            sent_at)
+          VALUES ($1, $2, $3, current_timestamp)
+          RETURNING id, from_username, to_username, body, sent_at`,
+      [from_username, to_username, body]
+    );
     return result.rows[0];
   }
 
-  /** Update read_at for message */
-
   static async markRead(id) {
     const result = await db.query(
-        `UPDATE messages
-           SET read_at = current_timestamp
-           WHERE id = $1
-           RETURNING id, read_at`,
-        [id]);
+      `UPDATE messages
+         SET read_at = current_timestamp
+         WHERE id = $1
+         RETURNING id, read_at`,
+      [id]
+    );
 
     if (!result.rows[0]) {
       throw new ExpressError(`No such message: ${id}`, 404);
@@ -43,33 +32,19 @@ class Message {
     return result.rows[0];
   }
 
-  /** Get: get message by id
-   *
-   * returns {id, from_user, to_user, body, sent_at, read_at}
-   *
-   * both to_user and from_user = {username, first_name, last_name, phone}
-   *
-   */
-
   static async get(id) {
     const result = await db.query(
-        `SELECT m.id,
-                m.from_username,
-                f.first_name AS from_first_name,
-                f.last_name AS from_last_name,
-                f.phone AS from_phone,
-                m.to_username,
-                t.first_name AS to_first_name,
-                t.last_name AS to_last_name,
-                t.phone AS to_phone,
-                m.body,
-                m.sent_at,
-                m.read_at
+      `SELECT m.id, m.from_username, f.first_name AS from_first_name,
+                f.last_name AS from_last_name, f.phone AS from_phone,
+                m.to_username, t.first_name AS to_first_name,
+                t.last_name AS to_last_name, t.phone AS to_phone,
+                m.body, m.sent_at, m.read_at
           FROM messages AS m
             JOIN users AS f ON m.from_username = f.username
             JOIN users AS t ON m.to_username = t.username
           WHERE m.id = $1`,
-        [id]);
+      [id]
+    );
 
     let m = result.rows[0];
 
@@ -97,6 +72,5 @@ class Message {
     };
   }
 }
-
 
 module.exports = Message;
